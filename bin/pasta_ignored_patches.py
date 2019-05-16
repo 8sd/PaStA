f9d43afad3fbd25b6d499449e6ec20dcf2c61d0e
8sd <git@sebdu.de>
2019-05-16 13:53:34 +0200

    First iteration of ignored patch analysis

--- bin/pasta_ignored_patches.py
+++ bin/pasta_ignored_patches.py
@@ -10,44 +10,70 @@ This work is licensed under the terms of the GNU GPL, version 2. See
 the COPYING file in the top-level directory.
 """
 
-import os
-import sys
 from pypasta import *
+from logging import getLogger
+
+log = getLogger(__name__[-15:])
+
+def get_relevant_subthread(thread, msg):
+    subthreads = thread.children
+    for subthread in subthreads:
+        if subthread.name == msg:
+            return subthread
+        res = get_relevant_subthread(subthread, msg)
+        if res is not None:
+            return res
+    return None
+
+
+def patch_has_no_response(thread):
+    return len(thread.children) == 0
+
+
+def patch_was_only_answered_by_author (thread, author, mbox):
+    for subthread in thread.children:
+        msg = mbox.get_messages(subthread.name)
+        if msg.author != author:
+            return False
+        if not patch_was_only_answered_by_author(subthread, author, mbox):
+            return False
+    return True
+
 
 def ignored_patches(config, prog, argv):
-    parser = argparse.ArgumentParser(prog=prog,
-                                      description='Check consistency of mailbox '
-                                                  'result')
-
-    args = parser.parse_args(argv)
-
     if config.mode != config.Mode.MBOX:
         log.error('Only works in Mbox mode!')
         return -1
 
     found = list()
-    patches = list()
+    mbox = Mbox(config)
+    threads = mbox.load_threads()
 
     # Load patches
-    ## load inbox
-    mbox = Mbox(config)
-
-    ##extract patches 
-    print(len(mbox.message_ids()))
-    for message_id in mbox.message_ids(): # This can be parallized
-        try:
-            patches.append(mbox.__getitem__(message_id))
-        except:
-            continue
-    print (len(patches))
-
+    cluster = Cluster.from_file("resources/linux/resources/mbox-result")
+    cluster.optimize()
+    patches = cluster.get_untagged () # Load all patches without commit hash
     # Iterate patches
-    for patches in patches:
+    for patch in patches:
+        if patch is None:
+            log.info()
+            continue
+        log.info ('Checking patch ' + patch)
         # check if patch has mail
-            # → ignore
-        # check if patch is merged
-            # → ignore
-        # → patch was ignored
-        found.append(patches)
+        thread = threads.get_thread(patch) # Return AnyTree of thread, containing the relevant msg
+        relevant_subthread = get_relevant_subthread(thread, patch) # Extract relevant subtree from tree
+        if relevant_subthread is None:
+            log.warning('No subthread for ' + patch + 'found!')
+            continue
+        if patch_has_no_response(relevant_subthread):
+            found.append(patch)
+            continue
+
+#       msg = mbox.get_messages(patch)
+#       if patch_was_only_answered_by_author(relevant_subthread, msg.author, mbox):
+#            found.append(patch)
+#            continue
+
+    log.info ('  ↪ done')
     # Write to file
-    #print(found)
+    print(len(found))

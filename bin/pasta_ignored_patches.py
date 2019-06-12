e9ed721c35084d6fcf78a6bebcfdbb5bc3ca34f1
8sd <git@sebdu.de>
2019-06-12 19:38:33 +0200

    Bug and lint fixes

--- bin/pasta_ignored_patches.py
+++ bin/pasta_ignored_patches.py
@@ -29,9 +29,12 @@ _statistic = {
     'patch set': set(),
     'large cluster': set(),
     'similar patch': set(),
-    'analyzed patches':set()
+    'analyzed patches': set(),
+    'un-ignored patch sets-versions': set(),
+    'ignored patch sets-versions': set(),
 }
 _patches = None
+_analyzed_patches = set()
 _threads = None
 
 
@@ -40,7 +43,7 @@ def write_and_print_statistic():
     for key, value in _statistic.items():
         file_name = (datetime.datetime.now().strftime('%Y.%m.%d-%H:%M_') + str(key) + ".out").replace(' ', '_')
         if type(value) is str:
-            log.info(str(key) + ': ' + value)
+            log.info(str(key) + ': ' + str(value))
         elif type(value) is set:
             log.info(str(key) + ': ' + str(len(value)))
             if len(value) < 100000:
@@ -60,7 +63,7 @@ def write_and_print_statistic():
 
 
 def was_similar_patch_merged(patch):
-    global _clusters
+    global _clusters  # TODO: Muss ich nicht via get_tagged() schauen ob etw gemerged ist?
     cluster = _clusters.get_cluster(_clusters.get_key_of_element(patch))
     if cluster is None:
         _statistic['error'].add(patch)
@@ -70,26 +73,18 @@ def was_similar_patch_merged(patch):
     return False
 
 
-def analyze_patch_set():
-    return  # TODO iterate over all patches from patch set
-
-
 def is_part_of_patch_set(patch):
     try:
         subject = _repo[patch].mail_subject
     except KeyError:
         return False
-    if type(re.search(r'[0-9]+\/[0-9]+\]', subject)) is type(None):
+    if type(re.search(r'[0-9]+/[0-9]+\]', subject)) is type(None):
         return False
     return True
     # TODO check if patch is part of patch set
 
 
-def has_versions(patch):
-    return False  # TODO check if entity (patch, patch set) has versions
-
-
-def get_author_of_msg (msg):
+def get_author_of_msg(msg):
     try:
         author = _repo[msg].author
     except KeyError:
@@ -135,32 +130,125 @@ def is_single_patch_ignored(patch):
     _statistic['ignored'].add(patch)
 
 
-def get_patch_set_of_patch(patch):
+def is_mail_cover_letter(patch):  # TODO
+    # if mail has no in reply to
+
+    # if mail has >1 children by same author
+    author = get_author_of_msg(patch)
+    children = _threads.get_thread(patch).children
+    mails_by_same_author = 0
+    for child in children:
+        if author is get_author_of_msg(child):
+            mails_by_same_author += 1
+
+    if mails_by_same_author < 2:
+        return False
+    pass
+
+
+def _get_patch_set_of_cover_letter(patch):
+    children = _threads.get_thread(patch).children
+    author = get_author_of_msg(patch)
+    result = set()
+
+    for child in children:
+        if author is get_author_of_msg(child):
+            result.add(child)
+    return result
+
+
+def _get_cover_letter_of_patch_set(patch):  # TODO
+    # get thread
+    # get node (patch)
+    # get parent-node
     return None
+
+
+def get_patch_set_of_patch(patch):
+    if not is_part_of_patch_set(patch):
+        return set()
+
+    if is_mail_cover_letter(patch):
+        return _get_patch_set_of_cover_letter(patch)
+    else:
+        return get_patch_set_of_patch(_get_cover_letter_of_patch_set(patch))
 
 
 def get_versions_of_patch(patch):
-    return None
+    global _clusters
+    cluster = _clusters.get_cluster(_clusters.get_key_of_element(patch))
+
+    if cluster is None:
+        _statistic['error'].add(patch)
+        return set()
+
+    if len(cluster) is 1:
+        return set()
+
+    author = get_author_of_msg(patch)
+    result = set()
+    for patch in cluster:
+        if get_author_of_msg(patch) is author:
+            result.add(patch)
+
+    return result
 
 
-def analyze_patch(patch, ignore_versions=False, ignore_patch_set=False):
-    if (not ignore_versions) and has_versions(patch):
+def get_versions_and_patch_sets(patch):
+    """
+    get_versions_and_patch_sets(patch) returns a set of all related patches.
+
+    The function collects all versions and patch sets as long as the result set grows.
+
+    :param patch: base patch
+    :return: set of all related patches
+    """
+
+    result_set = set()
+    current_iteration_set = set()
+
+    current_iteration_set.add(patch)
+
+    while True:
+        # Initialize next iteration
+        next_iteration_set = set()
+
+        for patch in current_iteration_set:
+            if patch in result_set:
+                continue  # to improve performance we can skip already analyzed patches
+
+            next_iteration_set |= get_patch_set_of_patch(patch)
+            next_iteration_set |= get_versions_of_patch(patch)
+
+        # Clean up
+        result_set |= current_iteration_set
+        if len(next_iteration_set) is 0:  # no new related patches found aborting analysis
+            return result_set
+        else:
+            current_iteration_set = next_iteration_set
+
+
+def analyze_patch(patch):
+    # Check if patch was already analyzed (e.g. it's part of an analyzed patch set) we can skip it
+    global _analyzed_patches
+    if patch in _analyzed_patches:
         return
-#        patches = get_versions_of_patch(patch)
-#        for v_patch in patches:
-#            analyze_patch(v_patch, True)
-#        _patches -= patches
-#        return
-    if (not ignore_patch_set) and is_part_of_patch_set(patch):
-        _statistic['patch set'].add(patch)
-        return
-#        patches = get_patch_set_of_patch(patch)
-#        for p_patch in patches:
-#            analyze_patch(p_patch, True, True)
-#        _patches -= patches
-#        return
-    # TODO: Handle version and patch set cases
-    is_single_patch_ignored(patch)
+
+    global _patches
+
+    # Get all related patches (versions, patch sets) of one patch
+    # If one related patch is not ignored all related patches count as not ignored
+    patches = get_versions_and_patch_sets(patch)
+
+    # Add patches which will be analyzed to analyzed patches set
+    # The patches which will be analyzed now cannot be already analyzed because the
+    # ↪ 'related'-relation is omnidirectional
+    _analyzed_patches |= patches
+    for tmp_patch in patches:
+        if not is_single_patch_ignored(tmp_patch):
+            _statistic['un-ignored patch sets-versions'] |= patches
+            return
+    _statistic['ignored patch sets-versions'] |= patches
 
 
 def ignored_patches(config, prog, argv):
@@ -176,6 +264,7 @@ def ignored_patches(config, prog, argv):
     global _patches
     global _threads
 
+    # Load config and required data
     config.time_frame = config.time_frame.replace(tzinfo=None)
     _config = config
 
@@ -197,4 +286,4 @@ def ignored_patches(config, prog, argv):
 
     write_and_print_statistic()
 
-    return True
+    return

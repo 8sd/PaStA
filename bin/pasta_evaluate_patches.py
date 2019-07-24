a6b403e56bb674f272f88595eac2a14c177a8cc2
Sebastian Duda <git@sebdu.de>
2019-07-24 10:44:34 +0200

    load minimum data

--- bin/pasta_evaluate_patches.py
+++ bin/pasta_evaluate_patches.py
@@ -10,5 +10,19 @@ This work is licensed under the terms of the GNU GPL, version 2. See
 the COPYING file in the top-level directory.
 """
 
+_clusters = None
+_config = None
+_repo = None
+
+
 def evaluate_patches(config, prog, argv):
-    pass
+    global _clusters
+    global _config
+    global _repo
+
+    _config = config
+    _repo = config.repo
+
+    _, _clusters = config.load_cluster()
+    _clusters.opimize()
+

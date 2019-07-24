a264a2dd0baf2c1233312e183c247f1b7c07ac1d
Sebastian Duda <git@sebdu.de>
2019-07-24 09:51:11 +0200

    store and load interim results with pickle

--- bin/pasta_get_maintainers.py
+++ bin/pasta_get_maintainers.py
@@ -25,10 +25,18 @@ _repo = ''
 _log = getLogger(__name__[-15:])
 _tag = ''
 _tags = ''
+_p = None
 
 _analyzed_files = dict()
 
 get_maintainers_args = ['perl', '../../../tools/get_maintainer.pl', '--subsystem', '--status', '--separator', ';;']
+
+
+def get_pool():
+    global _p
+    if _p is None:
+        _p = Pool(processes=cpu_count(), maxtasksperchild=10)
+    return _p
 
 
 def patch_date_extractor(patch):
@@ -142,6 +150,33 @@ def match_tag_patch(patch_id):
     return tag_of_patch, patch_id
 
 
+def build_tag_cache ():
+    _log.info('Parsing tags file')
+
+    tags = list()
+    patches_by_version = dict()
+    tags_file = open('resources/linux/tags', 'r')
+    for line in tags_file:
+        tag = line.split('\t')[0]
+        tags.append((tag, parser.parse(line.split('\t')[1][:-1])))
+        patches_by_version[tag] = set()
+
+
+    tags = sorted(tags, key=lambda x: x[1])
+    pickle.dump((tags, patches_by_version), open('resources/linux/tags.pkl', 'wb'))
+    return tags, patches_by_version
+
+
+def build_patch_tag_cache (patches_by_version, patches):
+    p  = get_pool()
+    return_value = p.map(match_tag_patch, tqdm(patches), 10)
+    for tag, result in return_value:
+        patches_by_version[tag].add(result)
+
+    pickle.dump(patches_by_version, open('resources/linux/tags_patches.pkl', 'wb'))
+    return patches_by_version
+
+
 def get_maintainers(config, prog, argv):
     global _log
     if config.mode != config.Mode.MBOX:
@@ -154,46 +189,46 @@ def get_maintainers(config, prog, argv):
     global _analyzed_files
 
     _config = config
-
     _repo = config.repo
+
     f_cluster, _clusters = config.load_cluster(must_exist=True)
     _clusters.optimize()
 
+    _log.info('loading tags…')
+    if 'build_tags' in argv:
+        _tags, patches_by_version = build_tag_cache()
+    else:
+        try:
+            _tags, patches_by_version = pickle.load(open('resources/linux/tags.pkl', 'rb'))
+            _log.info('loaded pickle')
+        except FileNotFoundError:
+            _tags, patches_by_version = build_tag_cache()
+
     _log.info('Loading patches…')
-    _patches = _clusters.get_untagged()
-    # patches_sorted = sorted(_patches, key=lambda p: patch_date_extractor(p))
-
-    _log.info('Parsing tags file')
-    tags = list()
-    patches_by_version = dict()
-    tags_file = open('resources/linux/tags', 'r')
-    for line in tags_file:
-        tag = line.split('\t')[0]
-        tags.append((tag, parser.parse(line.split('\t')[1][:-1])))
-
-        patches_by_version[tag] = set()
-
-    _tags = sorted(tags, key=lambda x: x[1])
+    patches = _clusters.get_untagged()
 
     _log.info('Match patches and tags…')
-    # parallel
-    p = Pool(processes=cpu_count(), maxtasksperchild=10)
-    return_value = p.map(match_tag_patch, tqdm(_patches), 10)
-    for tag, result in return_value:
-        patches_by_version[tag].add(result)
+    if 'map_patches_tags' in argv:
+        patches_by_version = build_patch_tag_cache(patches_by_version, patches)
+    else:
+        try:
+            patches_by_version = pickle.load(open('resources/linux/tags_patches.pkl', 'rb'))
+            _log.info('loaded pickle')
+        except FileNotFoundError:
+            patches_by_version = build_patch_tag_cache(patches_by_version, patches)
 
     result = dict()
-    for tag in tags:
+    for tag in _tags:
         tag = tag[0]
         if len(patches_by_version[tag]) == 0:
             continue
         # checkout git
         _log.info('Checking out tag: ' + tag)
         _repo.repo.checkout('refs/tags/' + tag)
-        _tag = tag
         _analyzed_files = dict()
 
         # parallel
+        p = get_pool()
         return_value = p.map(run_scripts, tqdm(patches_by_version[tag]), 10)
         for patch, res in return_value:
             result[patch] = res
